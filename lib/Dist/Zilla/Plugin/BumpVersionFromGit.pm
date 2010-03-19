d0c90ce574d3c141c85061fca9720eeee8217fba
David Golden <dagolden@cpan.org>
2010-03-19 16:06:00 -0400

    first draft works

--- lib/Dist/Zilla/Plugin/BumpVersionFromGit.pm
+++ lib/Dist/Zilla/Plugin/BumpVersionFromGit.pm
@@ -1,41 +1,94 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::BumpVersionFromGit;
-# ABSTRACT: a module for CPAN
+# ABSTRACT: provide a version number by bumping the last git release tag
 
-# Dependencies
-use autodie 2.00;
-use Moose 0.99;
+use Git::Wrapper;
+use version 0.80 ();
+
+use Moose;
 use namespace::autoclean 0.09;
 
-# extends, roles, attributes, etc.
+with 'Dist::Zilla::Role::VersionProvider';
 
-# methods
+# -- attributes
+
+has version_regexp  => ( is => 'ro', isa=>'Str', default => '^v(.+)$' );
+
+# -- role implementation
+
+sub provide_version {
+  my ($self) = @_;
+
+  require Version::Next;
+
+  # override (or maybe needed to initialize)
+  return $ENV{V} if exists $ENV{V};
+
+  my $git  = Git::Wrapper->new('.');
+  my $regexp = $self->version_regexp;
+
+  # find highest version from tags
+  my ($last_ver) =  sort { version->parse($b) <=> version->parse($a) }
+  grep { eval { version->parse($_) }  }
+  map  { /$regexp/ ? $1 : ()          }
+  $git->tag;
+
+  $self->log_fatal("Could not determine last version from tags")
+  unless defined $last_ver;
+
+  my $new_ver = Version::Next::next_version($last_ver);
+  $self->log("Bumping version from $last_ver to $new_ver");
+
+  $self->zilla->version("$new_ver");
+}
 
 __PACKAGE__->meta->make_immutable;
-
+no Moose;
 1;
 
 __END__
+
+=for Pod::Coverage::TrustPod
+    provide_version
 
 =begin wikidoc
 
 = SYNOPSIS
 
-  use Dist::Zilla::Plugin::BumpVersionFromGit;
+In your F<dist.ini>:
+
+    [BumpVersionFromGit]
+    version_regexp  = ^v(.+)$   ; this is the default
 
 = DESCRIPTION
 
-This module might be cool, but you'd never know it from the lack
-of documentation.
+This does the [Dist::Zilla::Role::VersionProvider] role.  It finds the last
+version number from tags and increments it as the new version used by
+Dist::Zilla.
 
-= USAGE
+The plugin accepts the following options:
 
-Good luck!
+* version_regexp - regular expression that matches a tag containing
+a version.  It should capture the version into $1.  Defaults to ^v(.+)$
+which matches the default tag from [Dist::Zilla::Plugin::Git::Tag]
+
+You can also set the {V} environment variable to override the new version.
+To bootstrap a version for a distribution that has not been released 
+(and thus not tagged), you need to use this or else set {version} in
+dist.ini (which should prevent this plugin from running).  After the first
+tagged release, you can remove {version} from dist.ini and let this module
+handle it for you.
+
+*NOTE* -- this module is a stop gap while Dist::Zilla is enhanced to
+allow more sophisiticated version number manipulation and may be
+deprecated in the future once those changes are complete.
 
 = SEE ALSO
 
-Maybe other modules do related things.
+* [Dist::Zilla::Plugin::VersionFromPrev] and 
+[Dist::Zilla::Plugin::Git::LastVersion] do something similar but in what
+I find to be a more complicated way of doing things
 
 =end wikidoc
 
---
+++ t/version-regexp.t
@@ -0,0 +1,56 @@
+#!perl
+
+use strict;
+use warnings;
+
+use Dist::Zilla     1.093250;
+use Git::Wrapper;
+use Path::Class;
+use Test::More      tests => 6;
+use Test::Exception;
+
+
+# build fake repository
+chdir( dir('t', 'version-regexp') );
+system "git init";
+my $git   = Git::Wrapper->new('.');
+$git->add(".");
+$git->commit({ message => 'import' });
+my ($zilla, $version);
+
+# with no tags and no initialization, should fail
+throws_ok {
+  $zilla = Dist::Zilla->from_config;
+  $version = $zilla->version;
+} qr/Could not determine last version from tags/, "fails when no tags";
+
+# initialize it
+{
+    local $ENV{V} = "1.23";
+    $zilla = Dist::Zilla->from_config;
+    is( $zilla->version, "1.23", "initialized with \$ENV{V}" );
+}
+
+# tag it
+$git->tag("release-v1.2.3");
+ok( (grep { /release-v1\.2\.3/ } $git->tag), "wrote v1.2.3 tag" );
+
+{
+    $zilla = Dist::Zilla->from_config;
+    is( $zilla->version, "v1.2.4", "initialized from last tag" );
+}
+
+# tag it
+$git->tag("release-1.23");
+ok( (grep { /release-1\.23/ } $git->tag), "wrote v1.23 tag" );
+
+{
+    $zilla = Dist::Zilla->from_config;
+    is( $zilla->version, "1.24", "initialized from last tag" );
+}
+
+
+# clean & exit
+dir( '.git' )->rmtree;
+exit;
+
